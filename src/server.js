ca550ddb82fc36b9ccfb9b4cd1217b8d29fafd3e
Anshgrover23 <ag5989670@gmail.com>
2024-12-17 13:18:52 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import path, {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// get the file path from url of the current module
+const __filename = fileURLToPath(import.meta.url)
+// get the directory name frm the file path
+const __dirname = dirname(__filename)
+//middleware
+app.use(express.json())
+
+// serves the file from public directory as static assests file
+app.use(express.static(path.join(__dirname, '../public')))
+
+// serving up the file form ./public directory
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port : ${PORT}`);
+    
+});
